af2526185c5b0b39a58b239d850ca4a8c1712347
Araviel <contact@araviel.io>
2021-12-22 20:53:45 +0100

    changing addresses

--- packages/token/src/instructions.ts
+++ packages/token/src/instructions.ts
@@ -15,11 +15,11 @@ import {
 } from '@project-serum/borsh';
 
 export const TOKEN_PROGRAM_ID = new PublicKey(
-  'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
+  'ToKLx75MGim1d1jRusuVX8xvdvvbSDESVaNXpRA9PHN',
 );
 
 export const WRAPPED_SOL_MINT = new PublicKey(
-  'So11111111111111111111111111111111111111112',
+  'Safe111111111111111111111111111111111111112',
 );
 
 export type TokenInstructionLayout =

bf0e80d3bb76c4d9c8345128afd75491a4762851
araviel-io <contact@araviel.io>
2022-03-12 12:25:58 +0100

    borsh occurences

--- packages/borsh/src/index.ts
+++ packages/borsh/src/index.ts
@@ -8,7 +8,7 @@ import {
   u8,
   union,
 } from 'buffer-layout';
-import { PublicKey } from '@solana/web3.js';
+import { PublicKey } from '@safecoin/web3.js';
 import BN from 'bn.js';
 export {
   u8,
